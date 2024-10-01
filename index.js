ee99fc1947b34237e7a14874efe9d252057fe898
sagar kashyap <kashyapsagarks21@gmail.com>
2024-10-01 15:30:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-let menu=document.getElementById('menu');
+let menu=document.getElementById("menu");
 menu.addEventListener('click',(()=>{
     let drop=document.getElementsByClassName('dropdown');
     let x = window.matchMedia("(mix-width: 769px)")
     if( drop[0].style.display !="none" ){
-
+        
         drop[0].style.display ="none";
     }else if( x.matches  ){
         drop[0].style.display ="none";
-
+        
     }
-     else{
+    else{
         drop[0].style.display="block";
         document.body.style.overflowY="hidden";
     }
+    let icon=document.querySelector('#menu i');
+    if(icon.className !="fa-solid fa-bars"){
+        icon.className ="fa-solid fa-bars"
+        document.body.style.overflowY="scroll";
+
+    }else{icon.className="fa-solid fa-xmark"}
+
 }))
 function myFunction(x) {
     let drop=document.getElementsByClassName('dropdown');
@@ -25,3 +32,5 @@ function myFunction(x) {
   x.addEventListener("change", function() {
     myFunction(x);
   });
+
+  
