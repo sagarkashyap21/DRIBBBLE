9c0f9552bc9fbbd85509fcd6e0e1dcbb9ebebb5a
sagar kashyap <kashyapsagarks21@gmail.com>
2024-10-01 16:25:22 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let menu=document.getElementById("menu");
 menu.addEventListener('click',(()=>{
     let icon=document.querySelector('#menu i');
-    if(icon.className !="fa-solid fa-bars"){
-        icon.className ="fa-solid fa-bars"
-        document.body.style.overflowY="scroll";
-    }else{icon.className="fa-solid fa-xmark"}
+    
     let drop=document.getElementsByClassName('dropdown');
     let x = window.matchMedia("(mix-width: 769px)")
     if( drop[0].style.display !="none" ){
@@ -12,19 +9,29 @@ menu.addEventListener('click',(()=>{
         drop[0].style.display ="none";
     }else if( x.matches  ){
         drop[0].style.display ="none";
+        icon.className ="fa-solid fa-bars"
         
     }
     else{
         drop[0].style.display="block";
         document.body.style.overflowY="hidden";
     }
+    if(icon.className !="fa-solid fa-xmark"){
+        icon.className ="fa-solid fa-xmark"
+        document.body.style.overflowY="hidden";
+    }else if(icon.className="fa-solid fa-bars"){
+        document.body.style.overflowY="scroll";
+
+    }
    
 
 }))
 function myFunction(x) {
     let drop=document.getElementsByClassName('dropdown');
+    let icon=document.querySelector('#menu i');
     if (x.matches) { // If media query matches
         drop[0].style.display ="none";
+        icon.className="fa-solid fa-bars"
     } 
   }
   var x = window.matchMedia("(min-width: 769px)")
